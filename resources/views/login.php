53f8e3b5704f3c388306a1767280aec6f227eca9
Meta Andrini Utari <metaandriniutari@Metas-MacBook-Pro.local>
2018-05-09 12:33:24 +1000

    Added login and register pages

--- resources/views/login.php
+++ resources/views/login.php
@@ -7,17 +7,85 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 </head>
 <body>
-<form>
-    <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="#" style="padding-left: 12vw;">UQ&A</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+        </ul>
     </div>
-    <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+</nav>
+
+<div style="padding-top: 10vh; padding-left: 13vw; padding-right: 13vw;">
+
+    <h1>UQ&A</h1>
+
+    <div class="card">
+        <div class="row">
+
+            <div class="col-sm-6">
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="loginEmail">Email Address</label>
+                            <input type="email" class="form-control" id="loginEmail" placeholder="s1234567@student.uq.edu.au">
+                        </div>
+                        <div class="form-group">
+                            <label for="loginPassword">Password</label>
+                            <input type="password" class="form-control" id="loginPassword" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-lg float-right" style="margin-bottom: 20px;">Login</button>
+                    </form>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="card-body">
+                    <p class="card-text" align="center">Don't have an account?</p>
+                    <button type="button" class="btn btn-primary btn-lg btn-block">Register Now</button>
+                </div>
+            </div>
+
+        </div>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+
+</div>
+
+<div class="footer">
+    <div style="background-color:#1a1a1a;padding-top:20px;padding-bottom:50px;">
+        <img src="images/FOOTER.png" width="1140" height="152" alt="Footer" usemap="#footer" class="logo">
+
+        <map name="footer">
+            <area shape="rect" coords="523,133,618,152" href="disclaimer.html" alt="Disclaimer">
+        </map>
+    </div>
+
+    <div style="height:10px;background-color:#FCCF2F;"></div>
+</div>
+
 </body>
 </html>
--- resources/views/register.php
+++ resources/views/register.php
@@ -7,17 +7,85 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 </head>
 <body>
-<form>
-    <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="#" style="padding-left: 12vw;">UQ&A</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+        </ul>
     </div>
-    <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+</nav>
+
+<div style="padding-top: 10vh; padding-left: 13vw; padding-right: 13vw;">
+
+    <h1>Register an Account</h1>
+
+    <div class="card">
+        <div class="row">
+
+            <div class="col-sm-3"></div>
+
+            <div class="col-sm-6">
+                <div class="card-body">
+                    <form>
+                        <div class="form-row">
+                            <div class="col">
+                                <label for="registerFirstName">First Name</label>
+                                <input type="text" class="form-control" id="registerFirstName" placeholder="First Name">
+                            </div>
+                            <div class="col">
+                                <label for="registerLastName">Last Name</label>
+                                <input type="text" class="form-control" id="registerLastName" placeholder="Last Name">
+                            </div>
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <label for="registerStudentNum">Student Number</label>
+                            <input type="number" class="form-control" id="registerStudentNum" placeholder="12345678">
+                        </div>
+                        <div class="form-group">
+                            <label for="registerEmail">Email Address</label>
+                            <input type="email" class="form-control" id="registerEmail" placeholder="s1234567@student.uq.edu.au">
+                        </div>
+                        <div class="form-group">
+                            <label for="registerPassword">Password</label>
+                            <input type="password" class="form-control" id="registerPassword" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-lg float-right" style="margin-bottom: 20px;">Create Account</button>
+                    </form>
+                </div>
+            </div>
+
+            <div class="col-sm-3"></div>
+
+        </div>
     </div>
-    <button type="submit" class="btn btn-primary">Register</button>
-</form>
+
+</div>
+
 </body>
 </html>
